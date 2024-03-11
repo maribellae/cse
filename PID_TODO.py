941d2b374d990604709af64cf48e789d71ae952e
maribellae <71653479+maribellae@users.noreply.github.com>
2024-03-11 20:46:00 +0300

    Update PID_TODO.py

--- PID_TODO.py
+++ PID_TODO.py
@@ -55,13 +55,13 @@ def coil_trajectory(z0, freq, duration,count_coils):
     x = list()
     y = list()
     z = list() 
-   
+    k = 0
     for i in range(freq):
         t0 = 2 * math.pi * (1.0/(freq-1)) * duration * i 
         x.append(0.8 * math.cos(t0/duration))
         y.append(0.8 * math.sin(t0/duration))
-        z.append(z0 + t0)
-        
+        z.append(z0 + k)
+        k+=0.01
     
     return (x,y,z)
 
@@ -87,7 +87,7 @@ if __name__ == '__main__':
 
     # (t , x , y,) = circle_trajectory(50 , 10 , 0.8)
     #(x , y, z) = square_trajectory(1 , 1.5)
-    (x , y, z) =coil_trajectory(1.5,50 , 10,4)
+    (x , y, z) = coil_trajectory(1.5,50 , 10,4)
     # Initialize the PID controller for x, y, and z
     pid_x = PIDController(x[0])
     pid_y = PIDController(y[0])
