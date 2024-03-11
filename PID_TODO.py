4a5815eb6db04778134d66dcf1335a2ea306b33c
maribellae <71653479+maribellae@users.noreply.github.com>
2024-03-11 21:15:45 +0300

    Update PID_TODO.py

--- PID_TODO.py
+++ PID_TODO.py
@@ -58,9 +58,9 @@ def coil_trajectory(z0, freq, duration,count_coils):
     z = list() 
     k = 0
     for i in range(freq *count_coils ):
-        t0 = 2 * math.pi * (1.0/(freq-1)) * duration * i * count_coils
-        x.append(0.8 * math.cos(t0/(duration * count_coils)))
-        y.append(0.8 * math.sin(t0/(duration * count_coils)))
+        t0 = 2 * math.pi * (1.0/(freq-1)) * duration * i 
+        x.append(0.8 * math.cos(t0/(duration)))
+        y.append(0.8 * math.sin(t0/(duration )))
         z.append(z0 + k)
         k+=0.5
     
